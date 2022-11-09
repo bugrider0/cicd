47d27e671907d1aa90859af17f5604ac5c7b9c4b
hsn0najafi <hsn0najafi@gmail.com>
2022-11-09 22:11:45 +0330

    feat(): add user ip to response

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    console.log(`----=+ Hello +=----`)
-    res.status(200).json({
-        name: `Fuck you`
-    })
+    console.log(req.ip)
+    res.status(200).json({ message: "WTF" })
 })
 
-app.listen(3000, () => console.log(`Server is running on PORT: 3000`))
+app.listen(3000, () => console.log(`Server is running on 0.0.0.0:3000`))
 
