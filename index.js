f9bf81cf5b59746feae2199dbd2831465e842955
hsn0najafi <hsn0najafi@gmail.com>
2022-11-09 18:21:31 +0330

    feat(): 1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/', (req, res) => {
     console.log(`----=+ Hello +=----`)
     res.status(200).json({
-        name: `Hossein Najafi --- this is plus`
+        name: `Fuck you`
     })
 })
 
