d67183f76d9faff3b96b9fabd3db181708224731
hsn0najafi <hsn0najafi@gmail.com>
2022-11-09 16:30:31 +0330

    feat(): CICD

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log(`-=+ Hello +=-`)
+    res.status(200).json({
+        name: `Hossein Najafi`
+    })
+})
+
+app.listen(3000, () => console.log(`Server is running on PORT: 3000`))
+
